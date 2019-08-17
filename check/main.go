029c96d468a0de349aa7423949b378c75fda5282
kc1r74p <7422353+kc1r74p@users.noreply.github.com>
2019-08-17 13:49:46 +0200

    Update main.go

--- check/main.go
+++ check/main.go
@@ -15,7 +15,17 @@ func main() {
 		fmt.Fprintln(os.Stderr, "parse error:", err.Error())
 		os.Exit(1)
 	}
-	versions := []models.InResponse{}
-	versions = append(versions, models.InResponse{Version: "Init=1"})
-	json.NewEncoder(os.Stdout).Encode(versions)
+
+	var keys map[string]string
+	keys["Init"] = "1"
+	//keys = append(keys, {"Init=1"})
+	json.NewEncoder(os.Stdout).Encode(models.OutResponse{
+		Version:  keys,
+	})
+
+	//versions := []models.OutResponse{}
+	//var keys []string
+	//keys = append(keys, "Init=1")
+	//versions = append(versions, models.OutResponse{Version: keys})
+	//json.NewEncoder(os.Stdout).Encode(versions)
 }
